a8613235b70d82b09e7b22e0132df83c08fdb39b
liliana-cc <lilicantero0508@gmail.com>
2025-04-06 20:54:46 -0400

    calendar page mock up with minor addition of event schema for backend use

--- konnekt_app/app/(tabs)/_layout.tsx
+++ konnekt_app/app/(tabs)/_layout.tsx
@@ -40,6 +40,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="calendarpage"
+        options={{
+          title: 'Calendar',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
     </Tabs>
   );
 }
